6f30f1e36cae13918a2118b2134f7c2476e0e4d6
Daphnis <iyahn@keti.re.kr>
2020-09-02 02:31:39 +0900

    - optimize code of change speed

--- thyme.js
+++ thyme.js
@@ -66,7 +66,7 @@ catch (e) {
 
 conf.running_type = 'local';        // 'local' or 'global' : When this is worked in Server, select 'global'
 
-conf.use_terminal = 'enable'; //'enable';
+conf.use_terminal = 'disable'; //'enable';
 
 
 if(conf.use_terminal === 'enable') {
